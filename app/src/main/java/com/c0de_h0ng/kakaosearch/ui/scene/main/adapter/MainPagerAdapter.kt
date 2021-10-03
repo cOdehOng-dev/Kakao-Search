7973548797df24784c101e3313faab28f9188b09
Oscar <ghdtjrdn0323@mergepoint.com>
2021-10-04 00:31:12 +0900

    [Fix] 메인 스크롤 버그 수정

--- app/src/main/java/com/c0de_h0ng/kakaosearch/ui/scene/main/adapter/MainPagerAdapter.kt
+++ app/src/main/java/com/c0de_h0ng/kakaosearch/ui/scene/main/adapter/MainPagerAdapter.kt
@@ -3,7 +3,6 @@ package com.c0de_h0ng.kakaosearch.ui.scene.main.adapter
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.c0de_h0ng.kakaosearch.base.BaseActivity
-import com.c0de_h0ng.kakaosearch.ui.scene.main.fragment.BlogFragment
 import com.c0de_h0ng.kakaosearch.ui.scene.main.fragment.IntegrateFragment
 
 /**
@@ -15,7 +14,7 @@ class MainPagerAdapter constructor(activity: BaseActivity<*>) : FragmentStateAda
 
     init {
         mainContentFragmentList.add(IntegrateFragment())
-        mainContentFragmentList.add(BlogFragment())
+        mainContentFragmentList.add(IntegrateFragment())
     }
 
     override fun getItemCount(): Int {
